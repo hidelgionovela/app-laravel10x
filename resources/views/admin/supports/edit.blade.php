406c8a765c1be11a0974e295eb08f9f618f6c38a
hidelgionovela <hidelgionovela@gmail.com>
2023-07-02 23:41:18 +0200

    update edit.blade.php in supports

--- resources/views/admin/supports/edit.blade.php
+++ resources/views/admin/supports/edit.blade.php
@@ -1,11 +1,20 @@
-@dd($support);
+{{-- @dd($support->id); --}}
 <h1>#{{ $support->id }} Actualizar duvida</h1>
 
-<form action="{{route('supports.store')}}" method="POST">
+@if ($errors->any())
+    @foreach ($errors->all() as $error)
+        {{ $error }}
+    @endforeach
+
+@endif
+
+<form action="{{route('supports.update', $support->id)}}" method="POST">
+
          {{-- <input type="hidden" value="{{csrf_token()}}" name="_token"> --}}
          {{-- @csrf  --}}
          {{-- Duas formas acima de incorporar o token nos formularios para verificacao de seguranca --}}
          @csrf
+         @method('PUT')
          <input type="text" name="subject" placeholder="Assunto" value="{{ $support->subject }}">
          <input name="body" id="" cols="30" rows="2" placeholder="Descricao" value="{{ $support->body }}">
          <input type="submit"  value="Enviar">
