d7e5de9312e24ac0c514ef1485578231c1534bb8
hidelgionovela <hidelgionovela@gmail.com>
2023-07-02 10:36:31 +0200

    update index.blade.php in supports

--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -1,4 +1,4 @@
-<h1>Listagem dos suportes</h1>
+<h1>Listagem das duvidas para Suporte</h1>
 <a href="{{route('supports.create')}}">adcionar suporte</a>
 
 <table style="width: 90%; border: solid black 1px;">
@@ -6,7 +6,8 @@
                   <th>Assunto</th>
                   <th>status</th>
                   <th>descricao</th>
-                  <th></th>
+                  <th>view</th>
+                  <th>edit</th>
          </thead>
          <tbody>
                   @foreach ($supports as $support)
@@ -14,9 +15,18 @@
                            <td>{{$support->subject}}</td>
                            <td>{{$support->status}}</td>
                            <td>{{$support->body}}</td>
-                           <td> > </td>
+                           <td> <a href="{{ route('supports.show', $support->id) }}">Ver</a> </td>
+                           <td> <a href="{{ route('supports.edit', $support->id) }}">Editar</a> </td>
                   </tr>
-                           
+
                   @endforeach
          </tbody>
 </table>
+
+<style>
+    td{
+        text-align: center;
+        border: solid black 1px;
+
+    }
+</style>
