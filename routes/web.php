137a434998268eea6caf8b7a674671979e7c3cce
hidelgionovela <hidelgionovela@gmail.com>
2023-07-02 10:35:52 +0200

    update web.php adding support routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 // use App\Http\Controllers\Admin\{SupportController}; // de forma a todas rotas que usam esse controller possam usar
-use App\Http\Controllers\SupportController; 
+use App\Http\Controllers\SupportController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,9 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/supports', [SupportController::class, 'index'])->name('supports');
-Route::post('/supports', [SupportController::class, 'store'])->name('supports.store');
+Route::get('/supports/{id}/edit', [SupportController::class, 'edit'])->name('supports.edit');
 Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
+Route::get('/supports/{id}', [SupportController::class, 'show'])->name('supports.show');
+Route::post('/supports', [SupportController::class, 'store'])->name('supports.store');
 Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
 
 
