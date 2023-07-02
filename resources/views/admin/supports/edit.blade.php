694749fe2cdc5d40c46d14e5db1614f69d4cb324
hidelgionovela <hidelgionovela@gmail.com>
2023-07-02 10:33:54 +0200

    create edit.blade.phph

--- resources/views/admin/supports/edit.blade.php
+++ resources/views/admin/supports/edit.blade.php
@@ -1,12 +1,13 @@
-<h1>Nova Duvida para suporte</h1>
+@dd($support);
+<h1>#{{ $support->id }} Actualizar duvida</h1>
 
 <form action="{{route('supports.store')}}" method="POST">
          {{-- <input type="hidden" value="{{csrf_token()}}" name="_token"> --}}
          {{-- @csrf  --}}
          {{-- Duas formas acima de incorporar o token nos formularios para verificacao de seguranca --}}
          @csrf
-         <input type="text" name="subject" placeholder="Assunto">
-         <input name="body" id="" cols="30" rows="2" placeholder="Descricao">
+         <input type="text" name="subject" placeholder="Assunto" value="{{ $support->subject }}">
+         <input name="body" id="" cols="30" rows="2" placeholder="Descricao" value="{{ $support->body }}">
          <input type="submit"  value="Enviar">
 </form>
 
